2c63812f6b891f306ba7e778ab76137f9ae1f575
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-08-07 10:38:21 +0300

    Create app.js

---
+++ Magic Eight Ball/app.js
@@ -0,0 +1,41 @@
+const userName = 'ANDRES';
+
+userName? console.log(`Hello, ${userName}`) : console.log('Hello');
+
+let userQuestion = 'Do I will find a job this month?';
+
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+  default:
+  break;
+}
+
+console.log(eightBall);
